c036b0bebf89177cb673ae79da779f8dbdaef71a
TaniaNawaz <60645627+TaniaNawaz@users.noreply.github.com>
2021-12-19 11:44:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from "./App.js"
+
+ReactDOM.render(
+<>
+ <App/>
+ {/* <Card name={Sdata[0].Name} 
+  image={Sdata[0].img} 
+  Email={Sdata[0].Email} 
+  phone={Sdata[0].Phone} 
+  company={Sdata[0].Company}
+  website={Sdata[0].Website}
+  Address={Sdata[0].Address}
+  />  */}
+  {/* 
+  {/* <br/>
+   <Card name={Sdata[1].Name} 
+  image={Sdata[1].img} 
+  Email={Sdata[1].Email} 
+  phone={Sdata[1].Phone} 
+  company={Sdata[1].Company}
+  website={Sdata[1].Website}
+  Address={Sdata[1].Address}
+  />
+  <br/> 
+   <Card name={Sdata[3].Name} 
+  image={Sdata[2].img} 
+  Email={Sdata[2].Email} 
+  phone={Sdata[2].Phone} 
+  company={Sdata[2].Company}
+  website={Sdata[2].Website}
+  Address={Sdata[2].Address}
+  /> 
+
+  <br/>
+   <Card name={Sdata[3].Name} 
+  image={Sdata[3].img} 
+  Email={Sdata[3].Email} 
+  phone={Sdata[3].Phone} 
+  company={Sdata[3].Company}
+  website={Sdata[3].Website}
+  Address={Sdata[3].Address}
+  />  */}
+ 
+</>,
+  document.getElementById('root')
+);
+
