7ecb7aa472dd7dc336885fae26ca841b35fd3184
TaniaNawaz <60645627+TaniaNawaz@users.noreply.github.com>
2021-12-19 11:43:54 +0500

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,59 @@
+const Sdata=[
+    {
+    img:"https://avatars.dicebear.com/v2/avataaars/Bret.svg?options[mood][]=happy",
+    Name:"Clementine Bauch",
+    Email:"Shanna@melissa.tv",
+    Phone:"010-692-6593 x09125",
+    Company:"Deckow-Crist",
+    Website:"anastasia.net",
+    Address:"Victor Plains, Suite 879, Wisokyburgh, 90566-7771",
+},
+{
+    img:"https://avatars.dicebear.com/v2/avataaars/Antonette.svg?options[mood][]=happy",
+    Name:"Ervin Howell",
+    Email:" Nathan@yesenia.net",
+    Phone: "1-463-123-4447",
+    Company: "Romaguera-Jacobson",
+    Website:" ramiro.info",
+    Address:"Victor Plains, Suite 879, Wisokyburgh, 90566-7771",
+
+},
+{
+    img:"https://avatars.dicebear.com/v2/avataaars/Karianne.svg?options[mood][]=happy",
+    Name:"Patricia Lebsack",
+    Email:" Julianne.OConner@kory.org",
+    Phone: "493-170-9623 x156",
+    Company: "Robel-Corkery",
+    Website: "kale.biz",
+    Address: "Hoeger Mall, Apt. 692, South Elvis, 53919-4257",
+},
+{
+    img:"	https://avatars.dicebear.com/v2/avataaars/Kamren.svg?options[mood][]=happy",
+    Name:"Chelsey Dietrich",
+    Email:"Lucio_Hettinger@annie.ca",
+    Phone: "(254)954-1289",
+    Company: "Keebler LLC",
+    Website:" demarco.info",
+    Address: "Skiles Walks, Suite 351, Roscoeview, 33263",
+},
+{
+    img:"	https://avatars.dicebear.com/v2/avataaars/Leopoldo_Corkery.svg?options[mood][]=happy",
+    Name:"Mrs. Dennis Schulist",
+    Email: "Karley_Dach@jasper.info",
+    Phone: "1-477-935-8478 x6430",
+    Company: "Considine-Lockman",
+    Website: "ola.org",
+    Address: "Skiles Walks, Suite 351, Roscoeview, 33263",
+},
+{
+    img:"https://avatars.dicebear.com/v2/avataaars/Moriah.Stanton.svg?options[mood][]=happy",
+    Name:"Clementina DuBuque",
+    Email:"Lucio_Hettinger@annie.ca",
+    Phone: "(254)954-1289",
+    Company: "Keebler LLC",
+    Website:" demarco.info",
+    Address: "Skiles Walks, Suite 351, Roscoeview, 33263",
+},
+]
+
+export default Sdata;
